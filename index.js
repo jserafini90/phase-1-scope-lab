0ecb04785b4f95c6d46d72d8dc0458aa322c4b16
Jessica Serafini <jpitts_kiheihs@yahoo.com>
2022-06-14 14:31:34 -0400

    commit tree 3c69b1aad056a7160c293eb2356a660055c0b49e

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return upperCaseCustomerName;
+}
+
+var bestCustomer;
+
+function setBestCustomer(){
+    return bestCustomer = 'not bob';
+    
+}
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+    
+}
+
+const leastFavoriteCustomer = 2;
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = 4;
+}
